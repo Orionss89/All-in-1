2ad54b6978ca8367c6df9103f3c7331fddf816d4
Lukasz <121812929+Orionss89@users.noreply.github.com>
2023-09-05 10:41:01 +0200

    Add files via upload

---
+++ Converter/src/Converter.java
@@ -0,0 +1,14 @@
+public class Converter {
+    public static void main(String[] args) {
+
+    ConverterFile con = new ConverterFile();
+    double a = 5.0;
+        System.out.println(a + "m");
+        System.out.println(con.cm(a) + "cm");
+        System.out.println(con.mm(a) + "mm");
+    
+
+
+
+    }
+}
---
+++ Converter/src/ConverterFile.java
@@ -0,0 +1,10 @@
+public class ConverterFile {
+    double cm(double a) {
+        return a*100;
+    }
+    double mm (double a){
+        return a*1000;
+
+    }
+
+}
