45601f42dc1ba4c23a90ca08b83e2c95b5d0d91c
Lukasz <121812929+Orionss89@users.noreply.github.com>
2023-09-06 17:40:19 +0200

    Update SumNumbers.java

--- SumNumbers/src/SumNumbers.java
+++ SumNumbers/src/SumNumbers.java
@@ -18,3 +18,8 @@ public class SumNumbers {
     }
 
 }
+
+//Napisz program, który najpierw pobiera od użytkownika liczbę mówiącą o tym ile liczb chce wprowadzić.
+//Następnie pobierz od niego tyle liczb ile zadeklarował, zsumuj
+//je i wyświetl na ekranie wynik. W programie wykorzystaj co najwyżej dwie zmienne
+//liczbowe (wliczając w to również potencjalne zmienne liczników pętli) i nie używaj tablic.
